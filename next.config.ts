1ae0c1a48fdf18b62bd1906f26f6fed0f2205f46
Franco Kasmenko <106505496+FrancoKasmenko@users.noreply.github.com>
2025-10-28 16:55:12 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: { unoptimized: true },  
+  trailingSlash: true,           
   reactCompiler: true,
-  output: 'export',
 };
 
 export default nextConfig;
