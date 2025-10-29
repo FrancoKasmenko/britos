d597e91c634ee08fb3e8edbcd718a4c9eb84aec8
Franco Kasmenko <106505496+FrancoKasmenko@users.noreply.github.com>
2025-10-29 12:54:14 -0300

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,9 +1,7 @@
 // app/robots.ts
+export const dynamic = 'force-static';
 export default function robots() {
   const base = "https://www.britos.com";
-  return {
-    rules: { userAgent: "*", allow: "/" },
-    sitemap: `${base}/sitemap.xml`,
-    host: base
-  };
+  return { rules: { userAgent: "*", allow: "/" }, sitemap: `${base}/sitemap.xml`, host: base };
 }
+
