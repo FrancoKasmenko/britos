8bde5381a9053cb20c3d1ca3809111a2034530a3
Franco Kasmenko <106505496+FrancoKasmenko@users.noreply.github.com>
2025-10-30 17:20:58 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+// src/app/page.tsx
 import HeroVideo from "../../components/HeroVideo";
 import Products from "../../components/Products";
 import About from "../../components/About";
@@ -8,17 +9,24 @@ export default function Page() {
   return (
     <main className="flex flex-col gap-24">
       <HeroVideo />
+
       <section id="productos" className="mx-auto w-full max-w-[1200px] px-4">
         <h2 className="text-3xl font-bold mb-6">Productos</h2>
         <Products />
       </section>
+
       <section id="about" className="max-w-6xl mx-auto px-4">
         <About />
       </section>
-      <section id="booksy" className="max-w-6xl mx-auto px-4" style="margin-top: -50px; margin-bottom: -50px;">
-  <BooksySection fallbackUrl="https://booksy.com/en-us/1347158_gaston-britos_barber-shop_123456_miami" />
-</section>
-            
+
+      <section
+        id="booksy"
+        className="max-w-6xl mx-auto px-4"
+        style={{ marginTop: -50, marginBottom: -50 }}
+      >
+        <BooksySection fallbackUrl="https://booksy.com/en-us/1347158_gaston-britos_barber-shop_123456_miami" />
+      </section>
+
       <section id="negocios" className="max-w-6xl mx-auto px-4 pb-24">
         <Ventures />
       </section>
